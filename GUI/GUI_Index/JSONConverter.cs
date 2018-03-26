e79d44d30afefce6a7dd55c6bdfa452682355455
maxdenstore <31626964+maxdenstore@users.noreply.github.com>
2018-03-26 13:08:43 +0200

    jsonconvert
    converter

---
+++ GUI/GUI_Index/JSONConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GUI_Index.Models;
+using Newtonsoft.Json;
+
+namespace GUI_Index
+{
+    public class JSONConverter
+    {
+
+        public string newUser(User newJas)
+        {
+            var jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(newJas);
+            return jsonString;
+        }
+
+        public string logInUser(User login)
+        {
+            var jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(login);
+            return jsonString;
+        }
+    }
+
+}
