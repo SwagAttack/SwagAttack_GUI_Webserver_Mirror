557ef85ec465e03ae7a89ab631ef4b78b66b8cb8
nicho1991 <ladefoged1991@gmail.com>
2018-04-18 21:57:33 +0200

    Opret lobby er ok, tilslut lobby skal vidergive hub info, lobby er ikke lavet

--- GUI/GUI_Index/wwwroot/js/Lobby.js
+++ GUI/GUI_Index/wwwroot/js/Lobby.js
@@ -3,15 +3,24 @@
 
 //lobby oprettet til liste LobbyList
 connection.on("OprettetLobby", (LobbyName) => {
+    
+    const li = document.createElement("li");
+    li.textContent = LobbyName;
+    li.id = "lobby";
+    //var button = document.createElement("button");
+    //button.innerText = LobbyName;
+    //button.class = "btn btn-post";
+    
+    li.addEventListener("click", function () { location.href = "Lobby" });
+    li.addEventListener("mouseover", function () { li.style.fontWeight = 'bold'; });
+    li.addEventListener("mouseleave", function () { li.style.fontWeight = 'normal'; });
+    //button.onclick = function onclick() {location.href = "Lobby"};
 
-    var button = document.createElement("button");
-    button.innerText = LobbyName;
-    button.class = "btn btn-post";
-
-    button.onclick = function onclick() {location.href = "Lobby"};
-
-    document.getElementById("LobbyList").appendChild(button);
+    document.getElementById("LobbyList").appendChild(li);
 });
+
+//Tilslut en exsisterende lobby ved hjælp af dens knap
+//document.getElementById()
 
 //opret lobby knap
 document.getElementById("OpretButton").addEventListener("click", event => {
