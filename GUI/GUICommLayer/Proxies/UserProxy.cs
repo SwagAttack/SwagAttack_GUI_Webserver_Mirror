2efb70374b2a000f7b8eaa26c737337a87841d5d
Malthe <malthedj@hotmail.com>
2018-04-19 10:33:21 +0200

    added concrete first draft implementation of UserProxy and LobbyProxy on GUI Communication layer.

--- GUI/GUICommLayer/Proxies/UserProxy.cs
+++ GUI/GUICommLayer/Proxies/UserProxy.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Domain.Interfaces;
 using GUICommLayer.Interfaces;
 
 namespace GUICommLayer.Proxies
 {
-    class UserProxy : IProxy
+    class UserProxy : IProxy<IUser>
     {
+        public Task<Uri> CreateInstance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IUser RequestInstance()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
