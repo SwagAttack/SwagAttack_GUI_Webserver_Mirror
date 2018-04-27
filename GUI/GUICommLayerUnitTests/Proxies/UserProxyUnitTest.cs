c0a3ab314a177642a0a035710d1e4d53efa29d84
jonasna <jonasna93@gmail.com>
2018-04-27 17:05:56 +0200

    Har opdateret userproxy

--- GUI/GUICommLayerUnitTests/Proxies/UserProxyUnitTest.cs
+++ GUI/GUICommLayerUnitTests/Proxies/UserProxyUnitTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using Domain.Interfaces;
@@ -14,13 +15,17 @@ namespace GUICommLayerUnitTests.Proxies
     [TestFixture]
     public class UserProxyUnitTest
     {
-        private IClientWrapper _clientFake;
+        private IHttpRequestFactory _fakeFactory;
+        private IHttpRequestBuilder _fakeBuilder;
         private IUser _userFake;
 
         [SetUp]
         public void Setup()
         {
-            _clientFake = Substitute.For<IClientWrapper>();
+            _fakeFactory = Substitute.For<IHttpRequestFactory>();
+            _fakeBuilder = Substitute.For<IHttpRequestBuilder>();
+            _fakeBuilder.SendAsync().Returns(new HttpResponseMessage() { StatusCode = HttpStatusCode.BadRequest });
+            _fakeBuilder.AddAuthentication(Arg.Any<string>(), Arg.Any<string>()).Returns(_fakeBuilder);
 
             _userFake = Substitute.For<IUser>();
             _userFake.Username = "TestUserName";
@@ -31,31 +36,64 @@ namespace GUICommLayerUnitTests.Proxies
         }
 
         [Test]
-        public void RequestInstance_ClientReceivedCallToGetInstance()
+        public void RequestInstance_CallsGetWithCorrectPath()
         {
             //arrange 
-            var uut = new UserProxy(_clientFake);
+            var uut = new UserProxy(_fakeFactory);
+            _fakeFactory.Get("api/User/Login").Returns(_fakeBuilder);
 
             //act
-            var result = uut.RequestInstanceAsync(_userFake.Username, _userFake.Password);
-            
-            //assert
+            var result = uut.RequestInstanceAsync(_userFake.Username, _userFake.Password).Result;
 
-            _clientFake.Received().GetInstance();
+            //assert
+            _fakeFactory.Received().Get("api/User/Login");
+            
         }
 
         [Test]
-        public void CreateInstance_ClientReceivedCallToGetInstance()
+        public void RequestInstance_CallsSendOnBuilder()
         {
             //arrange 
-            var uut = new UserProxy(_clientFake);
+            var uut = new UserProxy(_fakeFactory);
+            _fakeFactory.Get("api/User/Login").Returns(_fakeBuilder);
+            _fakeBuilder.SendAsync().Returns(new HttpResponseMessage() { StatusCode = HttpStatusCode.BadRequest });
 
             //act
-            var result = uut.CreateInstanceAsync(_userFake);
+            var result = uut.RequestInstanceAsync(_userFake.Username, _userFake.Password).Result;
 
             //assert
+            _fakeBuilder.Received(1).SendAsync();
 
-            _clientFake.Received().GetInstance();
+        }
+
+        [Test]
+        public void CreateInstance_CallsPostWithCorrectPath()
+        {
+            //arrange 
+            var uut = new UserProxy(_fakeFactory);
+            _fakeFactory.Post(Arg.Any<string>(), _userFake).Returns(_fakeBuilder);
+            _fakeBuilder.SendAsync().Returns(new HttpResponseMessage() { StatusCode = HttpStatusCode.BadRequest });
+
+            //act
+            var result = uut.CreateInstanceAsync(_userFake).Result;
+
+            //assert
+            _fakeFactory.Received(1).Post("api/User", _userFake);
+        }
+
+        [Test]
+        public void CreateInstance_CallsSendOnBuilder()
+        {
+            //arrange 
+            var uut = new UserProxy(_fakeFactory);
+            _fakeFactory.Post("api/User", _userFake).Returns(_fakeBuilder);
+            _fakeBuilder.SendAsync().Returns(new HttpResponseMessage() {StatusCode = HttpStatusCode.BadRequest});
+
+            //act
+            var result = uut.CreateInstanceAsync(_userFake).Result;
+
+            //assert
+            _fakeBuilder.Received(1).SendAsync();
         }
 
         #region How to test this in unittest ? mock api i webserver -> dette vil sige navngivning er anderledes? 
