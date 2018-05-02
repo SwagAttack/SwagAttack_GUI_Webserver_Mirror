63d6d46491c10b4a7ff982bb1f90e5a4918f2334
Gobbenobber <gobbenobber@gmail.com>
2018-05-02 18:43:46 +0200

    Hard referencing the csproj.

--- after_test.cmd
+++ after_test.cmd
@@ -1,5 +1,5 @@
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
 cd C:\projects\swagattack-gui-webserver-mirror\tools
-OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -register:user -targetargs:"vstest ..\GUI\GUI_Index.unit.test\WebserverUnitTests.csproj" -filter:"+[*]* -[*.Tests]*"
+OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -register:user -targetargs:"vstest C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\WebserverUnitTests.csproj" -filter:"+[*]* -[*.Tests]*"
 ::coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover
