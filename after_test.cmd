a57080689c807c9d18ba4c923e9dc5d84216d906
Gobbenobber <gobbenobber@gmail.com>
2018-05-02 21:07:47 +0200

    Should wurk now.

--- after_test.cmd
+++ after_test.cmd
@@ -1,6 +1,6 @@
 nuget install NUnit.Console -OutputDirectory tools
-nuget install OpenCover -Version 0.412.0 -OutputDirectory tools
-nuget install coveralls.net -Version 1.0.0-beta0001 -OutputDirectory tools
+nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
+nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
 cd C:\projects\swagattack-gui-webserver-mirror\tools
 REM nunit3-console C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\bin\Debug\netcoreapp2.0\WebserverUnitTests.dll --result=myresults.xml;format=AppVeyor
 REM OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -oldStyle -register:user -targetargs:"test C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\WebserverUnitTests.csproj --test-adapter-path:. --logger:Appveyor" -filter:"+[*]* -[*.Tests]*"
