cba3d927515813d9e497fc6d2df15d2fea838e25
Gobbenobber <gobbenobber@gmail.com>
2018-05-02 17:10:13 +0200

    virk

--- after_test.cmd
+++ after_test.cmd
@@ -1,6 +1,6 @@
-::nuget install NUnit.Console -OutputDirectory tools
+nuget install NUnit.Console -OutputDirectory tools
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
 cd C:\projects\swagattack-gui-webserver-mirror\tools
-OpenCover.4.6.519\tools\OpenCover.Console.exe -target:dotnet -register:user -targetargs:"test --no-build ..\GUI\GUI_Index.unit.test\bin\Debug\netcoreapp2.0\WebserverUnitTests.dll" -filter:"+[*]* -[*.Tests]*"
+OpenCover.4.6.519\tools\OpenCover.Console.exe -target:C:\Program Files\dotnet\dotnet.exe -register:user -targetargs:"test --no-build ..\GUI\GUI_Index.unit.test\bin\Debug\netcoreapp2.0\WebserverUnitTests.dll" -filter:"+[*]* -[*.Tests]*"
 ::coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover
