d4bea2c28affaa63a23d06cabe4ecccb329a9e29
Gobbenobber <gobbenobber@gmail.com>
2018-05-02 21:04:42 +0200

    This should bring coveralls for SwagAttack

--- after_test.cmd
+++ after_test.cmd
@@ -1,9 +1,9 @@
 nuget install NUnit.Console -OutputDirectory tools
-nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
+nuget install OpenCover -Version 0.412.0 -OutputDirectory tools
 nuget install coveralls.net -Version 1.0.0-beta0001 -OutputDirectory tools
 cd C:\projects\swagattack-gui-webserver-mirror\tools
 REM nunit3-console C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\bin\Debug\netcoreapp2.0\WebserverUnitTests.dll --result=myresults.xml;format=AppVeyor
 REM OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -oldStyle -register:user -targetargs:"test C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\WebserverUnitTests.csproj --test-adapter-path:. --logger:Appveyor" -filter:"+[*]* -[*.Tests]*"
 dotnet test C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\WebserverUnitTests.csproj --no-build --no-restore --logger:Appveyor /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 REM --test-adapter-path:.
-coveralls.net.1.0.0-beta0001\tools\csmacnz.Coveralls.exe --opencover -i C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\coverage.xml
+coveralls.net.0.412.0\tools\csmacnz.Coveralls.exe --opencover -i C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\coverage.xml
