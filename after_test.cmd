926e3778e00ec31a2550af62cfb2e36f31ee18dc
Gobbenobber <gobbenobber@gmail.com>
2018-05-02 20:48:17 +0200

    Latest coveralls version

--- after_test.cmd
+++ after_test.cmd
@@ -1,8 +1,8 @@
 nuget install NUnit.Console -OutputDirectory tools
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
-nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
+nuget install coveralls.net -OutputDirectory tools
 cd C:\projects\swagattack-gui-webserver-mirror\tools
 REM nunit3-console C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\bin\Debug\netcoreapp2.0\WebserverUnitTests.dll --result=myresults.xml;format=AppVeyor
 REM OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -oldStyle -register:user -targetargs:"test C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\WebserverUnitTests.csproj --test-adapter-path:. --logger:Appveyor" -filter:"+[*]* -[*.Tests]*"
-REM coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i .results.xml
-dotnet test C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\WebserverUnitTests.csproj --no-build --no-restore --logger:Appveyor /p:CollectCoverage=true REM --test-adapter-path:.
+dotnet test C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\WebserverUnitTests.csproj --no-build --no-restore --logger:Appveyor /p:CollectCoverage=true /p:CoverletOutputFormat=opencover REM --test-adapter-path:.
+coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i coverage.xml
