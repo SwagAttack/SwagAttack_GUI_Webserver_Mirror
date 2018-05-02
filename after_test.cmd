c58b8e0539b3fd7817cf2bd52c2967ef8a7e3828
Gobbenobber <gobbenobber@gmail.com>
2018-05-02 20:07:54 +0200

    Even more changes.

--- after_test.cmd
+++ after_test.cmd
@@ -1,4 +1,4 @@
-nuget install NUnit3 -OutputDirectory tools
+nuget install NUnit.Console -OutputDirectory tools
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
 cd C:\projects\swagattack-gui-webserver-mirror\tools
