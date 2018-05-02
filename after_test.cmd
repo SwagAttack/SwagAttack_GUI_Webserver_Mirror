f47a2d980d9756466a1a5d6bfb46b5e4479db19e
Gobbenobber <gobbenobber@gmail.com>
2018-05-02 20:17:20 +0200

    Testing these changes too

--- after_test.cmd
+++ after_test.cmd
@@ -2,6 +2,6 @@ nuget install NUnit.Console -OutputDirectory tools
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
 cd C:\projects\swagattack-gui-webserver-mirror\tools
-nunit3-console C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\bin\Debug\netcoreapp2.0\WebserverUnitTests.dll --result=myresults.xml;format=AppVeyor
-REM OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"nunit3-console.exe" -oldStyle -register:user -targetargs:"C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\WebserverUnitTests.csproj" -filter:"+[*]* -[*.Tests]*"
+REM nunit3-console C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\bin\Debug\netcoreapp2.0\WebserverUnitTests.dll --result=myresults.xml;format=AppVeyor
+OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -oldStyle -register:user -targetargs:"test C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\WebserverUnitTests.csproj --test-adapter-path:. --logger:Appveyor" -filter:"+[*]* -[*.Tests]*"
 REM coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i ./opencovertests.xml
