1c8f3396e51258b8a8790e18d36c820abf7e0ce9
Gobbenobber <gobbenobber@gmail.com>
2018-05-02 20:03:30 +0200

    More cahgnges to yaml

--- after_test.cmd
+++ after_test.cmd
@@ -1,5 +1,7 @@
+nuget install NUnit3 -OutputDirectory tools
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
 cd C:\projects\swagattack-gui-webserver-mirror\tools
-OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -oldStyle -register:user -targetargs:"test C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\WebserverUnitTests.csproj" -filter:"+[*]* -[*.Tests]*"
-coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover
+nunit3-console C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\bin\Debug\netcoreapp2.0\WebserverUnitTests.dll --result=myresults.xml;format=AppVeyor
+REM OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"nunit3-console.exe" -oldStyle -register:user -targetargs:"C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\WebserverUnitTests.csproj" -filter:"+[*]* -[*.Tests]*"
+REM coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i ./opencovertests.xml
