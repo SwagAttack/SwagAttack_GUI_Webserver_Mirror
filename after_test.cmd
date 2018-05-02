330469e1aedfb30ee38df87721a6d37037917198
Gobbenobber <gobbenobber@gmail.com>
2018-05-02 23:14:10 +0200

    This is correct,

--- after_test.cmd
+++ after_test.cmd
@@ -5,6 +5,4 @@ cd C:\projects\swagattack-gui-webserver-mirror\tools
 REM nunit3-console C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\bin\Debug\netcoreapp2.0\WebserverUnitTests.dll --result=myresults.xml;format=AppVeyor
 REM OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -oldStyle -register:user -targetargs:"test C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\WebserverUnitTests.csproj --test-adapter-path:. --logger:Appveyor" -filter:"+[*]* -[*.Tests]*"
 dotnet test C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\WebserverUnitTests.csproj --no-build --no-restore --logger:Appveyor /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
-cd coveralls.net.0.412\tools
-dir
 coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i ..\GUI\GUI_Index.unit.test\coverage.xml
