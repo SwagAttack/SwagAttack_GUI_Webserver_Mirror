eeb2fd4894e5a93616c081372c6f81cf53d99474
Max <maxbj@live.dk>
2018-05-06 14:51:34 +0200

    Test

--- GUI/GUI_Index/Interfaces/IHomeController.cs
+++ GUI/GUI_Index/Interfaces/IHomeController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GUI_Index.Interfaces
 {
-    interface IHomeController
+    public interface IHomeController
     {
         /// <summary>
         /// LogIndView
---
+++ GUI/WebserverIntegrationTests/IT_Webserver.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.Core.Configuration;
+using Domain.Interfaces;
+using Domain.Misc;
+using Domain.Models;
+using GUICommLayer.Interfaces;
+using GUICommLayer.Proxies;
+using GUICommLayer.Proxies.Utilities;
+using GUI_Index;
+using GUI_Index.Controllers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using NSubstitute.Core.Arguments;
+using NUnit.Framework;
+using GUI_Index.Interfaces;
+using GUI_Index.Session;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Rest;
+using RestEase;
+
+namespace WebserverIntegrationTests
+{
+
+   
+
+
+    public class IntegrationTest_ServerProvider
+    {
+
+        public static IUserProxy FakeUserProxy = Substitute.For<IUserProxy>();
+        public static ILobbyProxy FakeLobbyProxy = Substitute.For<ILobbyProxy>();
+        public static IUserSession FakeUserSession = Substitute.For<IUserSession>();
+        public static IHttpContextAccessor FakeAccessor = Substitute.For<IHttpContextAccessor>();
+        public static IHttpRequestFactory FakeFactory = Substitute.For<IHttpRequestFactory>();
+
+        
+
+        public IntegrationTest_ServerProvider(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddTransient<IUserProxy>(provider => FakeUserProxy);
+            services.AddTransient<ILobbyProxy>(provider => FakeLobbyProxy);
+
+
+            services.AddScoped<IUserSession>(provider => FakeUserSession);
+            services.AddSingleton<IHttpContextAccessor>(provider => FakeAccessor);
+
+            var sp = services.BuildServiceProvider();
+            services.AddTransient<IHttpRequestFactory, HttpRequestFactory>(f =>
+                new HttpRequestFactory(sp.GetService<IClientWrapper>(),
+                    "api/Users"));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseMvc();
+        }
+    }
+
+
+
+    [TestFixture]
+    public class IntegrationTest1
+    {
+        private static TestServer _server;
+        private static HttpClient _client;
+        private readonly User user = new User();
+        private readonly User atrrUser = new User();
+
+        private IHomeController _fakeHomeController;
+        private IHttpRequestFactory _fakeHttpRequestFactory;
+        private IHttpRequestBuilder _fakeHttpRequestBuilder;
+        private IUserProxy _fakeUserProxy;
+        private IHttpRequestFactory _fakeFactory;
+        private readonly IUser _person = Substitute.For<IUser>();
+
+
+        //public interface IUsersApi
+        //{
+        //    [Get("/api/User")]
+        //    Task<IEnumerable<User>> GetUserAsync(ushort mFilter);
+        //}
+
+        [SetUp]
+        public void Setup()
+        {
+            // setup testserver
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+            _server.Host.Start();
+            _client = _server.CreateClient();
+
+            //TestUser
+            user.Username = "apiuserh";
+            user.Password = "Maxmaxmax";
+            user.Email = "apiuser@123.dk";
+            user.GivenName = "Apiuser";
+            user.LastName = "Apiuser";
+
+
+            //TestPersUI
+            _person.Username = "apiuserh";
+            _person.Password = "Maxmaxmax";
+            _person.Email = "apiuser@123.dk";
+            _person.GivenName = "Apiuser";
+            _person.LastName = "Apiuser";
+
+            //Setup Homecontroller
+
+            _fakeHomeController = Substitute.For<IHomeController>();
+            _fakeHttpRequestFactory = Substitute.For<IHttpRequestFactory>();
+            _fakeHttpRequestBuilder = Substitute.For<IHttpRequestBuilder>();
+            _fakeUserProxy = Substitute.For<IUserProxy>();
+            _fakeHttpRequestFactory = Substitute.For<IHttpRequestFactory>();
+
+
+            IntegrationTest_ServerProvider.FakeUserProxy.CreateInstanceAsync(_person);
+            IntegrationTest_ServerProvider.FakeFactory.Post(Arg.Any<string>(), Arg.Any<object>());
+            _fakeUserProxy = IntegrationTest_ServerProvider.FakeUserProxy;
+            _fakeHttpRequestFactory = IntegrationTest_ServerProvider.FakeFactory;
+        }
+
+
+        //[Produces("application/json")]
+        //[Route("api/User")]
+        //[HttpGet("Login", Name = "GetUser")]
+        //public User GetUser([FromHeader] string username, [FromHeader] string password)
+        //{
+        //    atrrUser.Username = username;
+        //    atrrUser.Password = password;
+        //    return (atrrUser);
+        //}
+
+        
+
+        //Test if user was logged in
+        [Test]
+        public async Task UsersApi_LoginUser()
+        {                   
+            ////act
+            var sendUser = _fakeUserProxy.RequestInstanceAsync(user.Username, user.Password);
+            sendUser.Wait();
+
+            //var request = "api/User/Login";
+            //var response = await _client.GetAsync(request);
+
+            
+            ////assert
+            Assert.That(sendUser.Result, Is.Not.Null);
+        }
+
+        [Test]
+        public async Task UsersApi_Create_User()
+        {
+            ////act
+
+            var sendUser = _fakeUserProxy.CreateInstanceAsync(user);
+            sendUser.Wait();
+
+            ////assert
+
+
+            Assert.AreEqual(sendUser.Result, Is.Null);
+        }
+
+    }
+
+
+
+    
+}
